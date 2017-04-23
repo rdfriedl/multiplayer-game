88f4ec868be5fc69cddde7f8610be64a3c96250d
creeperdom <creeprdom@gmail.com>
2017-04-23 17:34:31 -0500

    Added gun cooldown to player

--- Assets/player/PlayerController.cs
+++ Assets/player/PlayerController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
+	
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+	private float bulletCooldown;
+	public float bulletInterval = 0.5f;
 
 	void Start () {
-		
+		bulletCooldown = bulletInterval;
 	}
 
     void Update() {
@@ -20,8 +23,12 @@ public class PlayerController : NetworkBehaviour {
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            CmdFire();
+		if(bulletCooldown < bulletInterval)
+			bulletCooldown += Time.deltaTime;
+		
+		if (Input.GetKey(KeyCode.Space) && bulletCooldown >= bulletInterval) {
+			CmdFire ();
+			bulletCooldown = 0;
         }
     }
 
